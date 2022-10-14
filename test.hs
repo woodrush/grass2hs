6bad71243eb21c34b035c2eda25e3e7b16c8c069
woodrush <woodrush924@gmail.com>
2022-10-14 18:22:02 +0900

    Types

--- test.hs
+++ test.hs
@@ -2,17 +2,19 @@ import Data.Char (ord, chr)
 
 nil a b = b
 
-data G a = Func a | Char Int
+data G a = Func a | Char a
 
 -- prim_in :: IO Int
 -- prim_in = do
 --     x <- getChar
 --     return $ ord x
 
-prim_out :: Int -> IO (t -> Int)
-prim_out x = do
-    print $ chr x
-    return (\a -> x)
+-- prim_out :: IO (G (t1 -> Int)) -> IO (G (t2 -> Int))
+prim_out x = case x of
+    Char x' -> do
+        print $ chr (x' prim_w)
+        return 0
+    otherwise -> return 0
 
 
 -- prim_succ :: G t -> IO Int
@@ -22,8 +24,8 @@ prim_out x = do
 --         \a -> do return $ mod (x + 1) 256
 --     otherwise -> return 0
 
-prim_w :: (G a -> G b) -> G c
-prim_w _ = Char (ord 'w')
-    
+-- prim_w :: (G a -> G b) -> G c
+prim_w = Char (\x -> ord 'w')
 
-main = prim_out 65
+
+main = prim_out prim_w
