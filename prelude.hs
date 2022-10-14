beaa4172ccc251056e6bdcbbdc1fe3fb8fa78a31
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-15 03:06:22 +0900

    Flush output

--- prelude.hs
+++ prelude.hs
@@ -1,6 +1,7 @@
 import Data.Char (ord, chr)
 import Control.Exception
 import System.IO.Error
+import System.IO
 
 data G = F (IO G -> IO G) | Char Int | In | Out | Succ
 
@@ -11,31 +12,32 @@ g :: IO G -> IO G -> IO G
 g x y = do
     x <- x
     case x of
-        F x' -> do
-            x' y
-        Char x' -> do
-            y <- y
-            case y of
-                Char y' -> do if x' == y' then true else nil
-                otherwise -> nil
-        Succ -> do
-            y <- y
-            case y of
-                Char y' -> return $ Char $ mod (y' + 1) 256
-                otherwise -> error "Non-Char type applied to Succ" 
-        Out -> do
-            y <- y
-            case y of
-                Char y' -> do
-                    putChar $ chr y'
-                    return y
-                otherwise -> error "Non-Char type applied to Out"
         In -> catch
             (do
                 c <- getChar
                 return $ Char $ ord c)
             (\e -> case e of
                 _ | isEOFError e -> y)
+        F x' -> do
+            x' y
+        otherwise -> do
+            y <- y
+            case x of
+                Char x' -> do
+                    case y of
+                        Char y' -> do if x' == y' then true else nil
+                        otherwise -> nil
+                Succ -> do
+                    case y of
+                        Char y' -> return $ Char $ mod (y' + 1) 256
+                        otherwise -> error "Non-Char type applied to Succ" 
+                Out -> do
+                    case y of
+                        Char y' -> do
+                            putChar $ chr y'
+                            hFlush stdout
+                            return y
+                        otherwise -> error "Non-Char type applied to Out"
 
 prim_in :: IO G
 prim_in = return In
