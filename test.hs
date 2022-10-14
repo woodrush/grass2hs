ea515235cbdac8d8bfe8b22ca0407e7656eb02cc
woodrush <woodrush924@gmail.com>
2022-10-14 21:27:47 +0900

    Types

--- test.hs
+++ test.hs
@@ -2,7 +2,7 @@ import Data.Char (ord, chr)
 
 nil a b = b
 
-data G a = Func a | Char a | ExtractChar
+data G a = Func a | Char a | CInt a | ExtractChar
 
 
 -- -- prim_in :: IO (Char Int)
@@ -17,8 +17,11 @@ prim_out x = do
     case x of
         (Char x') -> do
             let x = x' ExtractChar
-            print $ chr x
-            return $ prim_char x
+            case x of
+                CInt x' -> do
+                    print $ chr x'
+                    return $ prim_char x'
+                otherwise -> return $ prim_char 0
         otherwise -> return $ prim_char 0
 
 
@@ -28,15 +31,17 @@ prim_succ x = do
     case x of
         (Char x') -> do
             let x = x' ExtractChar
-            return $ prim_char (mod (x + 1) 256)
+            case x of
+                CInt x' -> return $ prim_char (mod (x' + 1) 256)
+                otherwise -> return $ prim_char 0
         otherwise -> return $ prim_char 0
 
 
 prim_char c = Char (\x -> case x of
-    ExtractChar -> c
-    otherwise -> 0)
+    ExtractChar -> (CInt c)
+    otherwise -> (CInt 0))
 
-prim_w :: IO (G (G t -> Int))
+prim_w :: IO (G ((G t) -> (G Int)))
 prim_w = return (prim_char (ord 'w'))
 
-main = prim_out $ prim_succ prim_in
+main = prim_out $ prim_succ (nil prim_w prim_in)
