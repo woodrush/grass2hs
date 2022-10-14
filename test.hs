1b1f6ee849172bc2bc01dda8c89102b2a7f32c6a
woodrush <woodrush924@gmail.com>
2022-10-14 16:09:35 +0900

    WIP: types

--- test.hs
+++ test.hs
@@ -9,10 +9,10 @@ data G a = Func a | W a
 --     x <- getChar
 --     return $ ord x
 
-prim_out :: G (IO Int) -> IO Int
+prim_out :: G (G () -> IO Int) -> IO Int
 prim_out x = case x of
     W z -> do
-            c <- z
+            c <- z (W ())
             -- let c = 65
             print $ chr c
             return c
@@ -23,8 +23,8 @@ prim_out x = case x of
 --     x <- x
 --     return $ mod (x + 1) 256
 
-prim_w :: IO Int
-prim_w = return $ ord 'w'
+prim_w :: G () -> IO Int
+prim_w _ = return $ ord 'w'
 -- case a of
 --     W t -> return $ ord 'w'
 --     otherwise -> return 0
