df32de1be51ddd734909daa7355a65f3c15aed74
woodrush <woodrush924@gmail.com>
2022-10-14 20:26:13 +0900

    Extract char

--- test.hs
+++ test.hs
@@ -2,12 +2,12 @@ import Data.Char (ord, chr)
 
 nil a b = b
 
-data G a = Func a | Char a
+data G a = Func a | Char a | ExtractChar
 
--- prim_in :: IO (Char Int)
-prim_in = do
-   x <- getChar
-   return (Char (ord x))
+-- -- prim_in :: IO (Char Int)
+-- prim_in = do
+--    x <- getChar
+--    return (Char (ord x))
  
 
 -- prim_out :: IO (G (t1 -> Int)) -> IO (G (t2 -> Int))
@@ -15,20 +15,21 @@ prim_out x = do
     x <- x
     case x of
         (Char x') -> do
-            print $ chr x'
+            let x = x' ExtractChar
+            print $ chr x
         otherwise -> return ()
 
 
--- prim_succ :: G t -> IO Int
-prim_succ x = do
-    x <- x
-    case x of
-        (Char x') -> return (Char (mod (x' + 1) 256))
-        otherwise -> return (Char 0)
+-- -- prim_succ :: G t -> IO Int
+-- prim_succ x = do
+--     x <- x
+--     case x of
+--         (Char x') -> return (Char (mod (x' + 1) 256))
+--         otherwise -> return (Char 0)
 
-prim_w :: IO (G Int)
-prim_w = return (Char (ord 'w'))
+-- prim_w :: IO (G Int)
+prim_w = return (Char (\x -> case x of
+    ExtractChar -> (ord 'w')
+    otherwise -> 0))
 
-main = prim_out (prim_succ (nil prim_w prim_in))
-
---(prim_succ prim_in)
+main = prim_out prim_w
