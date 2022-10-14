2e8fb1fcecba789a8fa464a5d60d287cb07958d7
woodrush <woodrush924@gmail.com>
2022-10-14 23:42:13 +0900

    Refactoring

--- test.hs
+++ test.hs
@@ -7,13 +7,13 @@ data G = F (G -> G) | Char Int | In | Out | Succ
 true = return $ F $ \x -> F $ \y -> x
 nil = return $ F $ \x -> F $ \y -> y
 
-gapply :: IO G -> IO G -> IO G
-gapply x y = do
+g :: IO G -> IO G -> IO G
+g x y = do
     x <- x
     case x of
         F x' -> do
             y <- y
-            return (x' y)
+            return $ x' y
         Char x' -> do
             y <- y
             case y of
@@ -22,32 +22,38 @@ gapply x y = do
         Succ -> do
             y <- y
             case y of
-                Char y' -> return (Char (mod (y' + 1) 256))
+                Char y' -> return $ Char $ mod (y' + 1) 256
                 otherwise -> error "Non-Char type applied to Succ" 
         Out -> do
             y <- y
             case y of
                 Char y' -> do
-                    print $ chr y'
+                    putChar $ chr y'
                     return y
                 otherwise -> error "Non-Char type applied to Out"
         In -> catch
             (do
                 c <- getChar
-                return (Char (ord c)))
+                return $ Char $ ord c)
             (\e -> case e of
                 _ | isEOFError e -> y)
-
-prim_w :: IO G
-prim_w = return (Char (ord 'w'))
-
-prim_out :: IO G
-prim_out = return Out
-
-prim_succ :: IO G
-prim_succ = return Succ
 
 prim_in :: IO G
 prim_in = return In
 
-main = (gapply prim_out (gapply prim_succ (gapply (gapply nil prim_w) (gapply prim_in prim_w))))
+prim_w :: IO G
+prim_w = return (Char (ord 'w'))
+
+prim_succ :: IO G
+prim_succ = return Succ
+
+prim_out :: IO G
+prim_out = return Out
+
+main = (g prim_out (g prim_succ (g (g nil prim_w) (g prim_in prim_w))))
+
+
+f0 = prim_in
+f1 = prim_w
+f2 = prim_succ
+f3 = prim_out
