5a5564d442a94ef8db6959d32bf640b3d1f73fb7
Hikaru Ikuta <woodrush924@gmail.com>
2022-10-15 15:27:47 +0900

    Stylization

--- prelude.hs
+++ prelude.hs
@@ -20,22 +20,22 @@ g x y = do
     x <- x
     y <- y
     case (x, y) of
-        (F x', _) -> x' $ ret y
+        (F x', _)          -> x' $ ret y
         (Char x', Char y') -> if x' == y' then true else nil
-        (Char x', _) -> nil
-        (Succ, Char y') -> ret $ Char $ mod (y' + 1) 256
-        (Succ, _) -> error "Non-Char type applied to Succ"
-        (Out, Char y') -> do
-                    putChar $ chr y'
-                    hFlush stdout
-                    ret y
-        (Out, _) -> error "Non-Char type applied to Out"
-        (In, _) -> catch
-            (do
-                c <- getChar
-                ret $ Char $ ord c)
-            (\e -> case e of
-                _ | isEOFError e -> ret y)
+        (Char x', _)       -> nil
+        (Succ, Char y')    -> ret $ Char $ mod (y' + 1) 256
+        (Succ, _)          -> error "Non-Char type applied to Succ"
+        (Out, Char y')     -> do
+                                putChar $ chr y'
+                                hFlush stdout
+                                ret y
+        (Out, _)           -> error "Non-Char type applied to Out"
+        (In, _)            -> catch
+                                (do
+                                    c <- getChar
+                                    ret $ Char $ ord c)
+                                (\e -> case e of
+                                    _ | isEOFError e -> ret y)
 
 prim_in :: IO G
 prim_in = ret In
